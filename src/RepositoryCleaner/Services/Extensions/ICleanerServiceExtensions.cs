32b1caae5e96035b7adf480cfe02eb8cc2a458b7
Geert van Horrik <geert@catenalogic.com>
2017-07-05 08:48:27 +0200

    Fix build

--- src/RepositoryCleaner/Services/Extensions/ICleanerServiceExtensions.cs
+++ src/RepositoryCleaner/Services/Extensions/ICleanerServiceExtensions.cs
@@ -21,40 +21,9 @@ namespace RepositoryCleaner.Services
     {
         private static readonly ILog Log = LogManager.GetCurrentClassLogger();
 
-        public static IEnumerable<Repository> GetCleanableRepositories(this ICleanerService cleanerService, params Repository[] repositories)
-        {
-            Argument.IsNotNull(() => cleanerService);
-
-            var cleanableRepositories = new List<Repository>();
-
-            foreach (var repository in repositories)
-            {
-                if (cleanerService.CanClean(repository))
-                {
-                    cleanableRepositories.Add(repository);
-                }
-            }
-
-            return cleanableRepositories;
-        }
-
-        public static async Task<IEnumerable<Repository>> GetCleanableRepositoriesAsync(this ICleanerService cleanerService, params Repository[] repositories)
-        {
-            Argument.IsNotNull(() => cleanerService);
-
-            return await Task.Factory.StartNew(() => GetCleanableRepositories(cleanerService, repositories));
-        }
-
-        public static async Task<bool> CanCleanAsync(this ICleanerService cleanerService, Repository repository)
-        {
-            Argument.IsNotNull(() => cleanerService);
-
-            return await Task.Factory.StartNew(() => cleanerService.CanClean(repository));
-        }
-
         public static async Task CleanAsync(this ICleanerService cleanerService, IEnumerable<Repository> repositories, bool isFakeClean, Action completedCallback = null)
         {
-            Argument.IsNotNull(() => cleanerService);
+            Argument.IsNotNull(nameof(cleanerService), cleanerService);
 
             var cleanedUpRepositories = new List<Repository>();
 
