baf8fe3f220f4fdc76097ae0c1dcf6b454ea5aea
Geert van Horrik <geert@catenalogic.com>
2020-07-21 15:12:37 +0200

    Code cleanup

--- src/RepositoryCleaner/App.xaml.cs
+++ src/RepositoryCleaner/App.xaml.cs
@@ -7,25 +7,28 @@
 
 namespace RepositoryCleaner
 {
-    using System;
-    using System.IO;
     using System.Windows;
     using System.Windows.Controls;
     using Catel.IoC;
     using Catel.Logging;
     using Catel.Reflection;
-    using Catel.Windows;
     using Catel.Windows.Threading;
     using Orchestra;
     using Services;
     using Views;
-    using AssemblyHelper = Orchestra.AssemblyHelper;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+#if DEBUG
+            LogManager.AddDebugListener(true);
+#endif
+        }
+
         /// <summary>
         /// Raises the <see cref="E:System.Windows.Application.Startup"/> event.
         /// </summary>
@@ -34,32 +37,10 @@ namespace RepositoryCleaner
         {
             base.OnStartup(e);
 
-            ThemeHelper.EnsureApplicationThemes(AssemblyHelper.GetEntryAssembly(), true);
-            //StyleHelper.CreateStyleForwardersForDefaultStyles();
-
-#if DEBUG
-            LogManager.AddDebugListener(true);
-#endif
+            this.ApplyTheme();
 
             // Show tooltips for 30 seconds
             ToolTipService.ShowDurationProperty.OverrideMetadata(typeof(DependencyObject), new FrameworkPropertyMetadata(30 * 1000));
-
-            var dependencyResolver = this.GetDependencyResolver();
-            var applicationInitializationService = dependencyResolver.Resolve<IApplicationInitializationService>();
-            var task = applicationInitializationService.Initialize();
-            task.ContinueWith(x => Dispatcher.BeginInvoke(ContinueStartup));
-        }
-
-        private void ContinueStartup()
-        {
-            //var assembly = AssemblyHelper.GetEntryAssembly();
-            //var logFileName = Path.Combine(assembly.GetDirectory(), string.Format("RepositoryCleaner_{0}.log", DateTime.Now.ToString("yyyyMMdd_HHmmss")));
-            //var fileLogListener = new FileLogListener(logFileName, 25 * 1024);
-
-            //LogManager.AddListener(fileLogListener);
-
-            var shell = new MainWindow();
-            shell.Show();
         }
     }
 }
